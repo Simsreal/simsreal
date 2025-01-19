e6bd04beda58a1f91823b0014d4a33bf07dc3c2e
spoonbobo <seasonluke@gmail.com>
2025-01-19 15:04:17 +0800

    updates

--- schema/environment.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum, auto
-
-
-class EnvDataType(Enum):
-    SIMULATOR = auto()
-    ROBOT = auto()
---
+++ schema/symbols.py
@@ -0,0 +1,7 @@
+from enum import IntEnum, auto
+
+
+class HumanStateSymbol(IntEnum):
+    FULLNESS = auto()
+    AGE = auto()
+    EMOTION = auto()
