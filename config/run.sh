31e924242550e3fe715f867bf5121db82e036d90
Jiang-Feiyu <msjfy223@connect.hku.hk>
2025-02-22 15:20:29 +0800

    fixed: hard code path

--- config/run.sh
+++ config/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python /mnt/e/simsreal/simsreal/config/WSL_ip.py &
-/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe "python E:\simsreal\simsreal\config\Windows_ip.py" &
+python ./config/WSL_ip.py &
+/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe "python .\config\Windows_ip.py" &
 wait
